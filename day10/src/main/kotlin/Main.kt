0b648336fc4d3e8962ce34cda0727ec33d82ce3f
Mike Neilens <mike.neilens@gmail.com>
2022-03-06 14:24:12 +0000

    day10

---
+++ day10/src/main/kotlin/Main.kt
@@ -0,0 +1,93 @@
+
+typealias Bots = MutableMap<Int, Bot>
+
+fun Bots.bot(number:Int) = getValue(number)
+
+typealias Bins = MutableMap<Int, Bin>
+
+fun Bins.bin(number:Int) =
+    if (contains(number)) getValue(number)
+    else {
+        val newBin = Bin(mutableListOf())
+        this[number] = newBin
+        newBin}
+
+enum class ReceiverType{Bot, Bin}
+
+data class Receiver(val type:ReceiverType, val number:Int)
+
+class Bot(val microchips:MutableList<Int>, val lowReceiver:Receiver, val highReceiver:Receiver, bots: Bots, bins: Bins  ) {
+
+    val addChip = { chip:Int -> addChip(chip, bots, bins)  }
+
+    private fun addChip(chip: Int, bots: Bots, bins: Bins) {
+        microchips.add(chip)
+        if (microchips.size == 2) {
+            addChipToBotOrBin(microchips.minOf { it },lowReceiver, bots, bins)
+            addChipToBotOrBin(microchips.maxOf { it },highReceiver, bots, bins)
+        }
+    }
+
+    private fun addChipToBotOrBin(chip:Int, receiver:Receiver, bots: Bots, bins: Bins) {
+        if (receiver.type == ReceiverType.Bot)
+            bots.bot(receiver.number).addChip(chip)
+        else
+            bins.bin(receiver.number).addChip(chip)
+    }
+
+    override fun toString(): String = "microchips=$microchips lowTo = $lowReceiver highTo = $highReceiver"
+
+    override fun equals(other: Any?): Boolean = other is Bot && microchips == other.microchips && lowReceiver == other.lowReceiver && highReceiver == other.highReceiver
+}
+
+data class Bin(val microchips:MutableList<Int>) {
+    val addChip = { chip: Int -> microchips.add(chip); Unit}
+}
+
+fun List<String>.createBots(bots: Bots, bins: Bins) {
+    forEach { string ->
+        if (string.startsWith("bot ")) {
+            val fromBotNumber = string.fromBot()
+            val bot = string.toBot(bots, bins)
+            bots[fromBotNumber] = bot
+        }
+    }
+}
+
+fun List<String>.assignValues(bots: Bots) {
+    forEach { string ->
+        if (string.startsWith("value")) {
+            val microchip = string.split(" ")[1].toInt()
+            val botNumber = string.split(" ")[5].toInt()
+            bots.bot(botNumber).addChip(microchip)
+        }
+    }
+}
+
+fun String.toBot(bots: Bots, bins: Bins ):Bot {
+    val lowToType = if (split(" ")[5] == "output") ReceiverType.Bin else ReceiverType.Bot
+    val lowToNumber = split(" ")[6].toInt()
+    val highToType = if (split(" ")[10] == "output") ReceiverType.Bin else ReceiverType.Bot
+    val highToNumber = split(" ")[11].toInt()
+    return Bot(mutableListOf(), Receiver(lowToType, lowToNumber), Receiver(highToType, highToNumber), bots, bins)
+}
+
+fun String.fromBot() = removePrefix("bot ").split(" ")[0].toInt()
+
+fun processInstructions(data:List<String>):Pair<Bots, Bins> {
+    val bots = mutableMapOf<Int, Bot>()
+    val bins = mutableMapOf<Int, Bin>()
+    data.createBots(bots, bins)
+    data.assignValues(bots)
+    return Pair(bots, bins)
+}
+
+fun partOne(data:List<String>, val1:Int, val2:Int):Int {
+    val (bots, _) = processInstructions(data)
+    return bots.toList().first { it.second.microchips.sorted() == listOf(val1, val2) }.first
+}
+
+fun partTwo(data:List<String>):Int {
+    val (_, bins) = processInstructions(data)
+    return bins.bin(0).microchips.first() * bins.bin(1).microchips.first() * bins.bin(2).microchips.first()
+}
--- day10/src/test/kotlin/MainTest.kt
+++ day10/src/test/kotlin/MainTest.kt
@@ -1,2 +1,72 @@
-class MainTest {
-}
+import io.kotest.core.spec.style.WordSpec
+import io.kotest.matchers.shouldBe
+
+class MainTest: WordSpec( {
+
+    "for part one" should {
+        val bots = mutableMapOf<Int,Bot>()
+        val bins = mutableMapOf<Int,Bin>()
+
+        val sampleData = """
+            value 5 goes to bot 2
+            bot 2 gives low to bot 1 and high to bot 0
+            value 3 goes to bot 1
+            bot 1 gives low to output 1 and high to bot 0
+            bot 0 gives low to output 2 and high to output 0
+            value 2 goes to bot 2
+        """.trimIndent().split("\n")
+
+        "convert a bot instruction into a bot when both receivers are bots" {
+            val bot = "bot 135 gives low to bot 27 and high to bot 166".toBot(bots, bins)
+            bot.lowReceiver.type shouldBe ReceiverType.Bot
+            bot.lowReceiver.number shouldBe 27
+            bot.highReceiver.type shouldBe ReceiverType.Bot
+            bot.highReceiver.number shouldBe 166
+        }
+        "convert a bot instruction into a bot when both receivers are bins" {
+            val bot = "bot 207 gives low to output 16 and high to output 8".toBot(bots, bins)
+            bot.lowReceiver.type shouldBe ReceiverType.Bin
+            bot.lowReceiver.number shouldBe 16
+            bot.highReceiver.type shouldBe ReceiverType.Bin
+            bot.highReceiver.number shouldBe 8
+        }
+        "convert a bot instruction into a bot when low receiver is a bot high receiver is a bin" {
+            val bot = "bot 208 gives low to bot 16 and high to output 8".toBot(bots, bins)
+            bot.lowReceiver.type shouldBe ReceiverType.Bot
+            bot.lowReceiver.number shouldBe 16
+            bot.highReceiver.type shouldBe ReceiverType.Bin
+            bot.highReceiver.number shouldBe 8
+        }
+        "convert a bot instruction into a bot when low receiver is a bin high receiver is a bot" {
+            val bot = "bot 209 gives low to output 16 and high to bin 8".toBot(bots, bins)
+            bot.lowReceiver.type shouldBe ReceiverType.Bin
+            bot.lowReceiver.number shouldBe 16
+            bot.highReceiver.type shouldBe ReceiverType.Bot
+            bot.highReceiver.number shouldBe 8
+        }
+        "convert sample instructions into bots" {
+            sampleData.createBots(bots, bins)
+            bots[2] shouldBe Bot(mutableListOf(),Receiver(ReceiverType.Bot,1), Receiver(ReceiverType.Bot,0),bots, bins)
+            bots[1] shouldBe Bot(mutableListOf(),Receiver(ReceiverType.Bin,1), Receiver(ReceiverType.Bot,0),bots, bins)
+            bots[0] shouldBe Bot(mutableListOf(),Receiver(ReceiverType.Bin,2), Receiver(ReceiverType.Bin,0),bots, bins)
+        }
+        "process instructions using sample data" {
+            val (resultingBots, resultingBins) = processInstructions(sampleData)
+            resultingBins[0]?.microchips?.first() shouldBe 5
+            resultingBins[1]?.microchips?.first() shouldBe 2
+            resultingBins[2]?.microchips?.first() shouldBe 3
+            resultingBots[2]?.microchips shouldBe listOf(5,2)
+        }
+        "find part one answer using sample data is 2" {
+            partOne(sampleData, 2, 5) shouldBe 2
+        }
+        "find part one answer using puzzleInput is 93 " {
+            partOne(puzzleInput, 17, 61) shouldBe 93
+        }
+    }
+    "for part two" should ({
+        "find answer using puzzle Input is 47101" {
+            partTwo(puzzleInput) shouldBe 47101
+        }
+    })
+})
